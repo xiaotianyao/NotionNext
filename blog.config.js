394c6672d3ef390ad419d0944e828e9bc51863b3
xiaotianyao <57620814+xiaotianyao@users.noreply.github.com>
2023-09-16 14:59:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    'STFangsong'
+    'FangSong'
     '"Segoe UI"',
     '"PingFang SC"',
     '-apple-system',
